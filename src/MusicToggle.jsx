1575bb1ec29a85b48c8a0368334873f766692421
María Romera <maguromera@gmail.com>
2025-04-10 20:02:46 -0300

    Mi primer componente :D

---
+++ src/MusicToggle.jsx
@@ -0,0 +1,27 @@
+import { useState, useRef } from "react";
+import Play from "./media/Play.svg";
+import Pause from "./media/Pause.svg";
+import BGMusic from "./media/background-music.mp3";
+
+export function MusicToggle(){
+    const [isPlaying, setIsPlaying] = useState(false)
+    const audioRef = useRef(new Audio(BGMusic))
+    const toggleMusicState = () => {
+        const audio = audioRef.current
+        if (isPlaying){
+            audio.pause()
+        } else {
+            audio.play()
+        }
+        setIsPlaying(!isPlaying);
+    }
+
+    return (
+        <button onClick={toggleMusicState} className="MusicToggle">
+            <img src={isPlaying ? Play : Pause} 
+            alt={isPlaying ? "Play music" : "No music"} 
+            className="MusicToggleIcon"/>
+        </button>
+    )
+
+}
